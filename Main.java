e0492eeec0d080b773a9198f3980f8dd28b8663b
helenkhim <73491230+helenkhim@users.noreply.github.com>
2021-01-08 14:02:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 public static void main(String[] args){
-long begin = 100;
-long plus = 1000 ;
+long begin = 100; //переменная отражает баланс до пополнения
+long plus = 1000 ; // пополнение
 long balance ;
 if (plus >= 1000) {
 balance = plus/100 + begin + plus;
